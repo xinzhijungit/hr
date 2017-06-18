da6aad948bc9c22f9ff9ac293fc435a89ead1c9b
'mldn' <'610207870@qq.com'>
2017-06-18 11:02:20 +0800

    增加hellocopy

---
+++ src/cn/mldn/demo/CopyOfHello.java
@@ -0,0 +1,9 @@
+package cn.mldn.demo;
+
+public class CopyOfHello {
+	public static void mian (String[] srgs){
+		System.out.print("Hello SVN");
+		System.out.print("Hello SVN");
+		System.out.print("Hello SVN");
+	}
+}
